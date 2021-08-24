264739e3558db2cf042f613f56d9b593bbd9b3f3
dani <dani.zaghian@gmail.com>
2021-08-24 10:21:02 -0700

    adds model with generator

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,13 +1,5 @@
-class Recipe
-  attr_accessor :title, :chef, :ingredients, :directions, :prep_time
-
-  def initialize(options)
-    @title = options[:title]
-    @chef = options[:chef]
-    @ingredients = options[:ingredients]
-    @directions = options[:directions]
-    @prep_time = options[:prep_time]
-  end
+class Recipe < ApplicationRecord
 end
 
-new_recipe = Recipe.new({title: "Raw Eggs", chef: "Jay Wengrow", ingredients: "One live Chicken", directions: "Squeeze the chicken"})
+recipe = Recipe.new(title: "Ice Cubes", chef: "Dani Zaghian", 
+ingredients: "Water", directions: "Place water in freezer", prep_time: 90)
