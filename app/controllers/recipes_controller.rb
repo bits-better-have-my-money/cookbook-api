89d470e1eec370a91e64fed6c394f5df45bed4d4
dani <dani.zaghian@gmail.com>
2021-08-27 10:00:44 -0700

    adds requests.http file and recipes create action

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -5,6 +5,10 @@ class RecipesController < ApplicationController
     render json: recipes.as_json
   end
 
+  def create
+    render json: {message: "Creating a recipe...."}
+  end
+
   def show
     recipe = Recipe.find(params[:id])
     render json: recipe.as_json
